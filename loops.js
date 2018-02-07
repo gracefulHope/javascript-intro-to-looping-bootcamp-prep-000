d8ec60b57652e4eb3c2c082fc878a0dcff9fa520
Sara S <ewf.gloria@gmail.com>
2018-02-07 20:57:24 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -1,3 +1,10 @@
-function forLoop(loopArray, i === 1, ){
-  
+function forLoop(loopArray){
+  for(let i = 0; i<25, i++){
+    if (i===1){
+      loopArray.push("I am 1 strange loop.")
+    } 
+    else{
+      loopArray.push("I am ${i} strange loops")
+    }
+  }
 }
